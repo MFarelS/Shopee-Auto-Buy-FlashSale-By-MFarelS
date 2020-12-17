de42d7bf15daa6003d75bda0cf81b6f8f765c99c
MFarelS <58540890+MFarelS@users.noreply.github.com>
2020-12-17 19:08:42 +0700

    Script

---
+++ index.js
@@ -0,0 +1,288 @@
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+setTimeout(function() {
+	console.log('SHOPEE AUTO BUY');
+}, 5000);
+setTimeout(function() {
+	console.log('');
+}, 6000);
+setTimeout(function() {
+	console.log('BY MFARELS');
+}, 7000);
+setTimeout(function() {
+	console.log('');
+}, 7000);
+setTimeout(function() {
+	console.log('YouTube : MFarelS CH');
+}, 7000);
+setTimeout(function() {
+	console.log('Instagram : @mfarelsyahtiawan');
+}, 7000);
+setTimeout(function() {
+	console.log('');
+}, 8000);
+setTimeout(function() {
+	console.log('FlashSale 1.1 Shopee');
+}, 9000);
+
+setTimeout(function() {
+	console.log('');
+}, 10000);
+setTimeout(function() {
+	console.log('');
+}, 10100);
+setTimeout(function() {
+	console.log('');
+}, 10200);
+setTimeout(function() {
+	console.log('');
+}, 10300);
+setTimeout(function() {
+	console.log('');
+}, 10400);
+setTimeout(function() {
+	console.log('');
+}, 10500);
+setTimeout(function() {
+	console.log('');
+}, 10600);
+setTimeout(function() {
+	console.log('');
+}, 10700);
+setTimeout(function() {
+	console.log('');
+}, 10800);
+setTimeout(function() {
+	console.log('');
+}, 10900);
+setTimeout(function() {
+	console.log('');
+}, 10100);
+setTimeout(function() {
+	console.log('');
+}, 10110);
+setTimeout(function() {
+	console.log('');
+}, 10090);
+setTimeout(function() {
+	console.log('');
+}, 10900);
+setTimeout(function() {
+	console.log('');
+}, 10999);
+
+setTimeout(function() {
+	console.log('Script Akan Dimulai Dalam Hitungan :');
+}, 15000);
+setTimeout(function() {
+	console.log('5');
+}, 16000);
+setTimeout(function() {
+	console.log('4');
+}, 17000);
+setTimeout(function() {
+	console.log('3');
+}, 18000);
+setTimeout(function() {
+	console.log('2');
+}, 19000);
+setTimeout(function() {
+	console.log('1');
+}, 20000);
+setTimeout(function() {
+	console.log('0');
+}, 21000);
+setTimeout(function() {
+	console.log('SCRIPT STARTING...');
+}, 25000);
+
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30000);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30100);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30200);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30300);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30400);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30500);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30600);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30700);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30800);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 30900);
+setTimeout(function() {
+	console.log('MENCARI BARANG...');
+}, 31000);
+
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32300);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32400);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32500);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32600);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32700);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32800);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 32900);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 33100);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 33200);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 33300);
+setTimeout(function() {
+	console.log('BARANG DITEMUKAN');
+}, 33400);
+
+setTimeout(function() {
+	console.log('Info :');
+}, 35000);
+setTimeout(function() {
+	console.log('FLASH SALE 1.1\n\nNama : IPHONE 11 PRO MAX\nHarga : Rp20.000\nPengiriman : JNE, JNT, GRAB\nDeskripsi : ...Baca Selengkapnya');
+}, 35100);
+setTimeout(function() {
+	console.log('MEMASUKAN KE KERANJANG');
+}, 35200);
+setTimeout(function() {
+	console.log('BERHASIL MEMASUKKAN BARANG KE KERANJANG');
+}, 36000);
+setTimeout(function() {
+	console.log(' ');
+}, 36000);
+setTimeout(function() {
+	console.log(' ');
+}, 36000);
+setTimeout(function() {
+	console.log('MEMILIH PPEMBAYARAN OTOMATIS');
+}, 36100);
+setTimeout(function() {
+	console.log('PEMBAYARAN BERHASIL DITETAPKAN : SHOPEEPAY\n\nSALDO SHOPEE : Rp50.000\n-Rp20.000 = Rp30.000');
+}, 37000);
+setTimeout(function() {
+	console.log(' ');
+}, 37000);
+setTimeout(function() {
+	console.log(' ');
+}, 37000);
+setTimeout(function() {
+	console.log('MEMILIH PENGIRIMAN OTOMATIS');
+}, 37100);
+setTimeout(function() {
+	console.log('PENGIRIMAN BERHASIL DITETAPKAN : GRAB');
+}, 38000);
+setTimeout(function() {
+	console.log(' ');
+}, 39000);
+setTimeout(function() {
+	console.log(' ');
+}, 39000);
+setTimeout(function() {
+	console.log('PEMBAYARAN SELESAI\n\nBARANG AKAN SEGERA DIKIRIMKAN');
+}, 40000);
+
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+setTimeout(function() {
+	console.log('SUKSES');
+}, 45000);
+
+setTimeout(function() {
+	console.log('TERIMA KASIH TELAH MENGGUNAKAN SCRIPT INI\n\n- MFarelS');
+}, 50000);
